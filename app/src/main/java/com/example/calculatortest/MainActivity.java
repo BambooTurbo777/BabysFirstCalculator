111c5aba9cc323b9f76e55d5fb04185cb71db2d8
BambooTurbo777 <BambooTurbo777@gmail.com>
2022-02-28 17:55:49 -0500

    Initial Push
    Layout made following Tim Buchalka's Android Class
    
    Classes set up by me

---
+++ app/src/main/java/com/example/calculatortest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.calculatortest;
+//Created during Android Java Masterclass - Become an App Developer by Tim Buchalka
+//David Smith, Feb 2022
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+
+    //Updates the editText inNum with the input from our buttons
+    public void calcutron5000(int num){
+
+    }
+    //Performs operations
+    public void calcutron5000(String op){
+
+    }
+    }
